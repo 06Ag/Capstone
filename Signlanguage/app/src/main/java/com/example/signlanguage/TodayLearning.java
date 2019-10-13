b02b37e6f3bf442ce4b25fdf768cb7e2df50954c
YukSangeun <tkddms150@naver.com>
2019-10-14 00:59:43 +0900

    today_learning.xml UI

--- Signlanguage/app/src/main/java/com/example/signlanguage/TodayLearning.java
+++ Signlanguage/app/src/main/java/com/example/signlanguage/TodayLearning.java
@@ -3,12 +3,18 @@ package com.example.signlanguage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TodayLearning extends AppCompatActivity {
+
+    TextView tv_UserNameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_learning);
+        tv_UserNameView = (TextView) findViewById(R.id.tv_userName);
+
+
     }
 }
--- Signlanguage/app/src/main/java/com/example/signlanguage/learning.java
+++ Signlanguage/app/src/main/java/com/example/signlanguage/learning.java
@@ -2,6 +2,9 @@ package com.example.signlanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class learning extends AppCompatActivity {
@@ -10,5 +13,15 @@ public class learning extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+
+        Button bt_today = findViewById(R.id.button);
+        bt_today.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final Intent intent = new Intent(getApplicationContext(), TodayLearning.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
     }
 }
