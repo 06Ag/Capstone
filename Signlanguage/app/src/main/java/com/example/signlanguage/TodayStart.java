825b069f27ce9e51e323952c8b35a9f2b0c21f45
YukSangeun <tkddms150@naver.com>
2019-10-14 21:23:15 +0900

    오늘의 학습 시작페이지 틀만듬

--- Signlanguage/app/src/main/java/com/example/signlanguage/TodayStart.java
+++ Signlanguage/app/src/main/java/com/example/signlanguage/TodayStart.java
@@ -7,19 +7,24 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.TextView;
 
 public class TodayStart extends AppCompatActivity {
 
     ViewPager pager;
-    int pos = 0;
+    Button bt_stop;
+    TextView tv_imageNum;
+    int pos = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_start);
 
+        tv_imageNum = findViewById(R.id.tv_image);
+        bt_stop = findViewById(R.id.bt_stop);
+
         //stop시 일일학습페이지로 넘어가며 현 학습한 단어 개수(임의로 pos) 반환
-        Button bt_stop = findViewById(R.id.bt_stop);
         bt_stop.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -29,6 +34,7 @@ public class TodayStart extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 
     //onclick속성이 지정된 view를 클릭시 자동으로 호출됨
@@ -36,16 +42,20 @@ public class TodayStart extends AppCompatActivity {
     //previous button, next button
     public void mOnClick(View v){
         if(v.getId() == R.id.bt_previous){
-            if(pos > 0)
-                pos = pos -1;
+            if(pos > 1) {
+                pos = pos - 1;
+                tv_imageNum.setText("수화 #" + Integer.toString(pos));
+            }
             else{
                 Toast toast = Toast.makeText(TodayStart.this, "처음 단어 입니다.", Toast.LENGTH_SHORT);
                 toast.show();
             }
         }
         else{
-            if(pos <10)
+            if(pos <10) {
                 pos = pos + 1;
+                tv_imageNum.setText("수화 #" + Integer.toString(pos));
+            }
             else{
                 Toast toast = Toast.makeText(TodayStart.this, "마지막 단어 입니다.", Toast.LENGTH_SHORT);
                 toast.show();
