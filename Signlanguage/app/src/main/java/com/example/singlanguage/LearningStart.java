e635d6c3ad7ffa675d143904b6a6004cc9607bcf
origin <iw76choi@naver.com>
2019-11-08 00:29:21 +0900

    오늘의 학습 구현

---
+++ Signlanguage/app/src/main/java/com/example/singlanguage/LearningStart.java
@@ -0,0 +1,52 @@
+package com.example.singlanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class LearningStart extends AppCompatActivity {
+    String date; //오늘의 날짜
+    int countword; //초기 학습 단어수
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_start);
+
+        final DBToday dbToday = DBToday.getInstance(getApplicationContext());
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        date = simpleDateFormat.format(new Date());//오늘날짜 가져오기
+
+        TextView today = (TextView) findViewById(R.id.start_day);
+        today.setText(date);
+
+        //라디오 버튼 값 가져오기
+        final RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
+        Button b = (Button)findViewById(R.id.learnstart);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = rg.getCheckedRadioButtonId();
+                //getCheckedRadioButtonId() 의 리턴값은 선택된 RadioButton 의 id 값.
+                RadioButton rb = (RadioButton) findViewById(id);
+                countword = Integer.parseInt(rb.getText().toString());
+                dbToday.insert(date,countword,0,0); //db에 현재 날짜와 학습 단어 수 삽입
+                Intent intent = new Intent(getApplicationContext(), learning.class);
+                startActivity(intent); //learning activity로 다시 돌아가기
+            } // end onClick()
+        });  // end Listener
+
+
+
+
+    }
+}
--- Signlanguage/app/src/main/java/com/example/singlanguage/learning.java
+++ Signlanguage/app/src/main/java/com/example/singlanguage/learning.java
@@ -1,14 +1,16 @@
 package com.example.singlanguage;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class learning extends AppCompatActivity {
 
@@ -16,6 +18,11 @@ public class learning extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+
+        final DBToday dbToday = DBToday.getInstance(getApplicationContext());
+
+        final String temp = dbToday.getDate();
+        System.out.println("DB저장 날짜: "+temp);
 
         //sign 검색 결과 페이지로 이동 SignSearch.java
         final EditText Name = (EditText) findViewById(R.id.searchtext);
@@ -32,16 +39,54 @@ public class learning extends AppCompatActivity {
             }
         });
 
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        final String date = simpleDateFormat.format(new Date());//오늘날짜 가져오기
+
         //일일학습 페이지로 이동 TodayLearning.java
         Button bt_today = findViewById(R.id.button);
         bt_today.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                final Intent intent = new Intent(getApplicationContext(), TodayLearning.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(intent);
+
+                //처음 학습 시작한 날 저장
+                if(temp == ""){
+                    Intent intent = new Intent(getApplicationContext(), LearningStart.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+
+                }else{
+                    String firstdate = dbToday.getDate(); //db에 저장되있는 처음 학습 날짜 불러옴
+                    int countword = dbToday.getCount(); //db에 저장되어있는 오늘의 학습 단어 수
+                    int day = 0; //학습한지 며칠째
+
+                    System.out.println("최초 학습 시작일 : " +firstdate);
+                    System.out.println("현재 학습일 : " +date);
+                    System.out.println("학습할 단어 수: " +dbToday.getCount());
+                    Date beginDate = null;
+                    Date endDate = null;
+                    try {
+                        beginDate = simpleDateFormat.parse(firstdate);
+                        endDate = simpleDateFormat.parse(date);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    // 시간차이를 시간,분,초를 곱한 값으로 나누면 하루 단위가 나옴
+                    long diff = endDate.getTime() - beginDate.getTime();
+                    long diffDays = diff / (24 * 60 * 60 * 1000);
+                    day = (int)diffDays;
+                    day += 1; //0일째면 1일째라고 하기 위해서 더해줌
+                    System.out.println("학습 일수 : "+ day);
+                    dbToday.updateday(countword,day);
+
+                    final Intent intent = new Intent(getApplicationContext(), TodayLearning.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                }
+
             }
         });
+
 
         //카테고리학습 페이지로 이동 CategoryLearning.java
         Button bt_category = findViewById(R.id.button2);
